dae3985762dd4b55b3d4e41bf436745428933664
MoZ0311 <mozmegane0311@gmail.com>
2026-03-01 23:16:36 +0900

    Application class追加/アプリケーション開始時の初期化処理実装

---
+++ App/src/core/Application.cpp
@@ -0,0 +1,28 @@
+﻿// Application class
+
+# include "Application.hpp"
+
+using namespace SceneSettings;
+
+Application::Application()
+	: m_sceneManager{}
+{
+	// ウィンドウサイズをFHDに変更し、フルスクリーン化
+	Window::Resize(ScreenSize);
+	Window::SetFullscreen(true);
+
+	// 全てのシーンを登録し、最初に読み込むシーンを設定
+	registerScenes();
+	// m_sceneManager.init(State::Title, TransitionDuration);
+}
+
+bool Application::run()
+{
+	// シーン内の更新、描画が成功したかを返す
+	return m_sceneManager.update();
+}
+
+void Application::registerScenes()
+{
+	Print << U"Register Scenes!";
+}
--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,17 @@
 ﻿# include <Siv3D.hpp>
+# include "App/src/core/Application.hpp"
 
 void Main()
 {
+	Application app{};
+
 	while (System::Update())
 	{
-
+		// ゲームループ中に更新に失敗した時
+		if (!app.run())
+		{
+			// アプリケーションを終了する
+			break;
+		}
 	}
 }
