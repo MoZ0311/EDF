f9ab9d9edba7da77025847cc41bed5f749f09238
MoZ0311 <mozmegane0311@gmail.com>
2026-03-04 23:43:44 +0900

    カメラ操作クラス追加

---
+++ App/src/player/PlayerCamera.cpp
@@ -0,0 +1,14 @@
+﻿// PlayerCamera class
+
+# include "PlayerCamera.hpp"
+
+PlayerCamera::PlayerCamera(const BasicCamera3D& camera)
+	: m_camera{ camera }
+{
+
+}
+
+void PlayerCamera::update()
+{
+
+}
--- App/src/player/PlayerInput.hpp
+++ App/src/player/PlayerInput.hpp
@@ -1,4 +1,6 @@
-﻿# pragma once
+﻿// PlayerInput class
+
+# pragma once
 
 class PlayerInput
 {
